5898359b7dd72f3245727006f4b5e7b298c4ff7c
moehriegitt <github@ndwun.de>
2024-10-09 09:37:40 +0200

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.theiling.neatlauncher"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.3"
+        versionCode = 5
+        versionName = "1.4"
     }
     buildTypes {
         release {
